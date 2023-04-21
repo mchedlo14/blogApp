155ae94b97a090230de7a5db7661f450155901e8
mchedlo14 <golocogo4@gmail.com>
2023-04-21 22:15:13 +0400

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react'
-import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom';
-import Home from './pages/Home';
-import CreatePost from './pages/CreatePost';
-import Login from './pages/Login';
-import './App.css'
-import {signOut} from 'firebase/auth'
-import { auth } from './Firbase';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import "./App.css";
+import { signOut } from "firebase/auth";
+import { auth } from "./Firbase";
 
 const App = () => {
-  const [isAuth,setIsAuth] = useState(false);
-
+  const [isAuth, setIsAuth] = useState(false);
 
   const signUserOut = () => {
     signOut(auth).then(() => {
-      localStorage.clear()
-      setIsAuth(false)
-      window.location.pathname = '/login'
-    })
-  }
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
 
   return (
     <Router>
       <nav>
-        <Link to='/'>Home</Link>
+        <Link to="/">Home</Link>
 
         {!isAuth ? (
           <Link to="/login"> Login </Link>
@@ -32,15 +31,14 @@ const App = () => {
             <button onClick={signUserOut}> Log Out</button>
           </>
         )}
-
       </nav>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/createpost' element={<CreatePost isAuth={isAuth}/>}/>
-        <Route path='/login' element={<Login setIsAuth={setIsAuth}/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
